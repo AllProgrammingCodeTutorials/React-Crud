0c2537cecac47cbb4c0ee214b4e3983b9e70fcdd
purnimagurung <grg.puru@gmail.com>
2021-09-25 11:56:53 +0200

    react-tutorialV1

---
+++ react-crud/src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { Home } from './components/Home';
+import { EditUser } from './components/EditUser';
+import { AddUser } from './components/AddUser';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+function App() {
+  return (
+    <div style={{maxWidth: "30rem", margin:"4rem auto" }}>
+      <Router>
+        <Switch>
+          <Route exact path="/home" component={Home} />
+          <Route path="/add" component={AddUser} />
+          <Route path="/edit/:id" component={EditUser} />
+        </Switch>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
